a70db4c8ae760e7d20b81fedb17037ca11f91c1b
arungopichand98@gmail.com <arung@ARUN>
2026-03-22 05:19:02 -0400

    JobController got added

---
+++ Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobTrackerApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JobController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllJobs()
+        {
+            var jobs = new List<string>
+            {
+                "Deloitee - .Net Developer",
+                "EY - Power Platform Engineer"
+            };
+            return Ok(jobs);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetJobById(int id)
+        {
+            return Ok($"Job with ID:{id}");
+        }
+
+    }
+}
